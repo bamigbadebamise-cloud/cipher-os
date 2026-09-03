344f2726f23ccd89bc3ad58a05cf2211ee262345
Bamigbade Bamise <bamigbadebamise@gmail.com>
2026-09-03 04:11:49 -0700

    Fix Turbopack configuration

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const withPWA = require('next-pwa')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // your existing config stays here
+  turbopack: {},
 };
 
 module.exports = withPWA(nextConfig);
